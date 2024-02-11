32b58290e77907210d2cea3d34182ab35858f82d
Felipe Calvo Béhèran <felipecalvo6@gmail.com>
2024-02-10 22:38:19 -0300

    Pendientes

--- Trakk-Base.py
+++ Trakk-Base.py
@@ -1,3 +1,9 @@
 # Construcción proyecto TRAKK
 ## Listas de tareas
 # [Check] Crear repositorio en GitHub.
+
+
+# Pendientes
+# Crear modelos Usuario, Personaje, Objetivos
+# Crear formulario de modelos y buscador
+# Crear acceso Superadmin
