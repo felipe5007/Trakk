7732eb2f1fdb52e508b4a52f86a710d90d47f12e
Felipe Calvo Béhèran <felipecalvo6@gmail.com>
2024-02-17 19:07:17 -0300

    update

--- trakk/appTrakk/models.py
+++ trakk/appTrakk/models.py
@@ -1,9 +1,30 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
-class Users(models.Model):
-    nombre = models.CharField(max_length=20)
-    edad = models.IntegerField()
-    email = models.EmailField()
+
+class Usuario(models.Model):
+    name = models.CharField(max_length=30, unique=False)
+    email = models.EmailField(unique=True)
+    ultimo_login = models.DateTimeField(auto_now=True)
+    esta_activa = models.BooleanField(default=True)
+    password = models.CharField(max_length=255)
+    check_password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
     
+class Hallazgo(models.Model):
+    name = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=250)
+    Estado = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+    
+class Feedback(models.Model):
+    name = models.CharField(max_length=150)
+    comentario = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.name}"
