e23fdb2f7cc931ec8ca4e78aba7a6cbc69724061
Felipe Calvo Béhèran <felipecalvo6@gmail.com>
2024-02-19 11:13:52 -0300

    update

--- trakk/appTrakk/urls.py
+++ trakk/appTrakk/urls.py
@@ -4,14 +4,8 @@ from .views import *
 urlpatterns = [
     path('', home, name="home.html"),
     path('usuarios/', usuarios, name="usuarios"),
-    path('agregar_usuario/', agregar_usuario, name="agregar_usuario"),
-    path('buscar_usuario/', buscar_usuario, name="buscar_usuario"),
     #
     path('feedback/', feedback, name="feedback"),
-    path('agregar_feedback/', agregar_feedback, name="agregar_feedback"),
-    path('buscar_feedback/', buscar_feedback, name="buscar_feedback"),
     #
     path('hallazgo/', hallazgo, name="hallazgo"),
-    path('agregar_hallazgo/', agregar_hallazgo, name="agregar_hallazgo"),
-    path('buscar_hallazgo/', buscar_hallazgo, name="buscar_hallazgo"),
 ]
--- trakk/appTrakk/views.py
+++ trakk/appTrakk/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.template import Template, Context
+from .models import *
 from .forms import *
 
 # Create your views here.
@@ -8,31 +9,47 @@ def home(request):
 
 # Usuarios
 def usuarios(request):
-    UsuarioForm = UsuarioForm()
-    return render(request, "appTrakk/usuarios.html", {"form": UsuarioForm})
-def agregar_usuario(request):
-    return render(request, "appTrakk/agregar_usuario.html")
+    if request.method == "POST":
+        UForm = UsuarioForm(request.post)
+        if UForm.is_valid():
+            user_name = UForm.cleaned_data.get("name")
+            user_email = UForm.cleaned_data.get("email")
+            usuario = Usuario(name=user_name, email=user_email)
+            usuario.save()
+            return render(request, "appTrakk/home.html")
+    else:
+        UForm = UsuarioForm()
+        return render(request, "appTrakk/usuarios.html", {"form": UForm})
 
-def buscar_usuario(request):
-    return render(request, "appTrakk/buscar_usuario.html")
 
 #Hallazgos
 def hallazgo(request):
-    return render(request, "appTrakk/hallazgo.html")
-def agregar_hallazgo(request):
-    HallazgoForm = HallazgoForm()
-    return render(request, "appTrakk/agregar_hallazgo.html", {"form": HallazgoForm})
+    if request.method == "POST":
+        HForm = HallazgoForm(request.post)
+        if HForm.is_valid():
+            hallazgo_name = HForm.cleaned_data.get("name")
+            hallazgo_descripcion = HForm.cleaned_data.get("descripcion")
+            hallazgo = Hallazgo(name=hallazgo_name, descripcion=hallazgo_descripcion)
+            hallazgo.save()
+            return render(request, "appTrakk/home.html")
+        
+    else:
+        HForm = HallazgoForm()
+    return render(request, "appTrakk/hallazgo.html", {"form": HForm})
 
-def buscar_hallazgo(request):
-    return render(request, "appTrakk/buscar_hallazgo.html")
 
 #Feedbacks
 def feedback(request):
-    return render(request, "appTrakk/feedback.html")
-
-def agregar_feedback(request):
-    FeedbackForm = FeedbackForm()
-    return render(request, "appTrakk/agregar_feedback.html",{"form": FeedbackForm})
-
-def buscar_feedback(request):
-    return render(request, "appTrakk/buscar_feedback.html")
+    if request.method == "POST":
+        FForm = FeedbackForm(request.post)
+        if FForm.is_valid():
+            feedback_name = FForm.cleaned_data.get("name")
+            feedback_comentario = FForm.cleaned_data.get("descripcion")
+            feedback_email = FForm.cleaned_data.get("email")
+            feedback = Feedback(name=feedback_name, descripcion=feedback_comentario, email=feedback_email)
+            feedback.save()
+            return render(request, "appTrakk/home.html")
+        
+    else:
+        FForm = FeedbackForm()
+    return render(request, "appTrakk/feedback.html",{"form": FForm})
